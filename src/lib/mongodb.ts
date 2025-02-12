3bacd8497319b419be98e4dcb84db0202e69bdca
Paul Lin <paullin@MacBookPro.lan>
2025-02-11 20:59:12 -0500

    Fix MongoDB client setup and move global declaration to top level

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,32 +1,30 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb'
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local');
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined
 }
 
-const uri = process.env.MONGODB_URI;
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
+}
+
+const uri = process.env.MONGODB_URI
+const options = {}
+
+let clientPromise: Promise<MongoClient>
 
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  declare global {
-    var _mongoClientPromise: Promise<MongoClient> | undefined
+  if (!global._mongoClientPromise) {
+    const client = new MongoClient(uri, options)
+    global._mongoClientPromise = client.connect()
   }
-  const globalWithMongo = global as typeof global & {
-    _mongoClientPromise: Promise<MongoClient> | undefined
-  };
-
-  if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri);
-    globalWithMongo._mongoClientPromise = client.connect();
-  }
-  clientPromise = globalWithMongo._mongoClientPromise;
+  clientPromise = global._mongoClientPromise
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri);
-  clientPromise = client.connect();
+  const client = new MongoClient(uri, options)
+  clientPromise = client.connect()
 }
 
-export default clientPromise;
+export default clientPromise
